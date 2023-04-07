cf685762b8bd5a785cef539b14de2b322cc2ce75
valeriiyachyrko <valeriia.chyrko@oa.edu.ua>
2023-04-07 11:26:17 +0300

    Laboratory 3. Controllers, routes, templates. Сreating and configuring a REST, BaseController, PostController controllers. Add a template with a header to the page http://blog.com/blog/posts.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +26,23 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::resource('rest', RestTestController::class)->names('restTest');
+
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
+});
+
+//Адмінка
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index','edit','store','update','create',];
+    Route::resource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
 });
