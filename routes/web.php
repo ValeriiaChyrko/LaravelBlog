35f03429aa264b00d117e64e8f2405252191133c
valeriiyachyrko <valeriia.chyrko@oa.edu.ua>
2023-05-23 11:46:20 +0300

    Laboratory 11. Collections. Make controller DiggingDeeperController. It retrieves a collection of blog posts using Eloquent and performs operations such as filtering, mapping, sorting, and appending/prepending items to the collection.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\DiggingDeeperController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,14 @@ use App\Http\Controllers\RestTestController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'digging_deeper'], function () {
+
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+        ->name('digging_deeper.collections');
+
 });
 
 Route::middleware([
